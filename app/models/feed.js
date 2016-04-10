db561d0d7dc711dc81be8fadf027e07ff1f7ee88
andrewsosa001 <andrewsosa001@gmail.com>
2016-04-10 15:42:50 -0400

    Removed old node/express stuff

--- app/models/feed.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var FeedSchema   = new Schema({
-    message: String,
-    authorID: String
-});
-
-module.exports = mongoose.model('Feed', FeedSchema);
