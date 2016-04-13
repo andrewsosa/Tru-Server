fef25c8927704d54e5129c461a271e91e5bbaba1
andrewsosa001 <andrewsosa001@gmail.com>
2016-04-13 07:45:17 -0400

    Reqs, snippets demo app

--- Tru/Tru/settings.py
+++ Tru/Tru/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'snippets.apps.SnippetsConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,10 +78,17 @@ WSGI_APPLICATION = 'Tru.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'tru.db',                                   # Or path to database file if using sqlite3.
+
+        # The following settings are not used with sqlite3:
+        'USER': 'tru-db-user',
+        'PASSWORD': 'forkingmaster',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
@@ -119,3 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
